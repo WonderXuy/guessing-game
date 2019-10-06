4894b8976316cc214e40c5216982c0ffcea2b91b
WonderXuy <veko.dmitriy@mail.ru>
2019-10-06 22:28:17 +0300

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return Math.ceil(this.min+Math.abs(this.min-this.max)/2);
     }
 
     lower() {
-
+        this.max = Math.ceil(this.min+Math.abs(this.min-this.max)/2);
     }
 
     greater() {
-
+        this.min += Math.ceil(Math.abs(this.min-this.max)/2);
     }
 }
 
